9fc483c3a0e18f18529886adb5a8a4e8861f8f68
Lucas Forte <sinistrosfamily@gmail.com>
2026-08-06 14:18:30 -0300

    Trabalho Pesquisa Herança

---
+++ JAVA/Trabalho Pesquisa Herança/Animal.java
@@ -0,0 +1,7 @@
+class Animal {
+    String nome;
+
+    void fazerSom() {
+        System.out.println("O animal faz um som");
+    }
+}
---
+++ JAVA/Trabalho Pesquisa Herança/Cachorro.java
@@ -0,0 +1,6 @@
+public class Cachorro extends Animal {
+
+    void latir() {
+        System.out.println("O cachorro late");
+    }
+}
